51d98ba73fc071ebba2e4da004e94b5d7282d2ed
0xclutch <marcus@mccarthy.com.au>
2024-09-04 06:53:48 +0000

    why isnt PWA working

--- components/HomePage.js
+++ components/HomePage.js
@@ -6,9 +6,8 @@ import Settings from "./pages/Settings";
 import ScanQR from "./pages/ScanQR";
 import Icon from 'react-native-vector-icons/Ionicons';
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Modal, Button } from 'antd-mobile';
 
-const HomePageContent = ({ navigation }) => {    
+const HomePageContent = ({ navigation }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [fakeLoading, setFakeLoading] = useState(true);
@@ -17,28 +16,13 @@ const HomePageContent = ({ navigation }) => {
   const [mName, setMName] = useState("");
   const [lName, setLName] = useState("");
   const [profilePicture, setProfilePicture] = useState(null);
-  const carBroomBroom = require("./pages/images/icon.png")
-
-
-  const requestNotificationPermission = () => {
-    if ('Notification' in window && navigator.serviceWorker) {
-      Notification.requestPermission().then(permission => {
-        if (permission === 'granted') {
-          console.log('Notification permission granted.');
-          // Proceed with setting up notifications
-        } else {
-          console.log('Denied notification permission.');
-        }
-      });
-      }
-  };
+  const carBroomBroom = require("./pages/images/icon.png");
 
   useEffect(() => {
     requestNotificationPermission();
     setTimeout(() => {
       setFakeLoading(false);
     }, 2500);
-
 
     const fetchUser = async () => {
       try {
@@ -90,121 +74,33 @@ const HomePageContent = ({ navigation }) => {
     return <Text>Loading...</Text>;
   }
 
-  const styles = StyleSheet.create({
-    background: {
-      flex: 1,
-      backgroundColor: '#e7e6ed', // Background color for the entire screen
-    },
-    profileContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginBottom: 20,
-      marginTop: 40,
-    },
-    profilePicture: {
-      width: 100,
-      height: 100,
-      borderRadius: 5,
-      marginRight: 20, // Space between image and text
-    },
-    legalNameContainer: {
-      flexDirection: 'column',
-    },
-    legalName: {
-      fontSize: 24,
-    },
-    bold: {
-      fontWeight: 'bold',
-      fontSize: 24,
-    },
-    header: {
-      fontSize: 24,
-      fontWeight: '400',
-      marginTop: 20,
-      marginBottom: 20,
-    },
-    safetyPadding: {
-      padding: 20,
-    },
-    button: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      backgroundColor: '#fff',
-      borderBottomColor: '#ccc',
-      borderBottomWidth: 1,
-      paddingVertical: 15,
-      paddingHorizontal: 20,
-    },
-    buttonText: {
-      fontSize: 18,
-      color: '#333',
-      flex: 1,
-      paddingLeft: 10,
-      alignItems: 'center',
-      textAlign: 'center',
-      alignContent: 'space-between',
-      flex: 1,
-    },
-    icon: {
-      width: 30,
-      height: 30,
-      alignItems: 'center',
-      alignContent: "center"
-    },
-    bannerColour: {
-      backgroundColor: '#972541',
-      width: '110%',
-      height: 70
-    },
-
-    loadingScreen: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#e7e6ed', // Background color for loading screen
-    },
-    loadingFont: {
-      fontSize: 16,
-      marginBottom: 50
-    },
-    modalContent: {
-      padding: 20,
-      backgroundColor: 'white',
-      borderRadius: 10,
-    },
-    modalText: {
-      fontSize: 16,
-    },
-  });
-
   return (
-    <SafeAreaView style={styles.background}>
-      <View style={styles.bannerColour}></View>
-      {fakeLoading && (
-        <View style={styles.loadingScreen}>
-          <Text style={styles.loadingFont}>Fetching your digital wallet</Text>
-          <ActivityIndicator size={50} color="#6a5964"/>
-        </View>
-      )}
-      {!fakeLoading && (
-        <View style={styles.safetyPadding}>
-          <View style={styles.profileContainer}>
-            {profilePicture && (
-              <Image style={styles.profilePicture} source={{uri: profilePicture}} />
-            )}
-            <View style={styles.legalNameContainer}>
-              <Text style={styles.legalName}>{fName} {mName}</Text>
-              <Text style={styles.bold}>{lName}</Text>
-            </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.bannerColour}></View>
+        {fakeLoading ? (
+          <View style={styles.loadingScreen}>
+            <Text style={styles.loadingFont}>Fetching your digital wallet</Text>
+            <ActivityIndicator size={50} color="#6a5964"/>
           </View>
-          
-          <Text style={styles.header}>Credentials</Text>
-          <TouchableOpacity style={styles.button} onPress={redirectToId}>
-            <Text style={styles.buttonText}><Image style={styles.icon} source={carBroomBroom} /> Drivers License</Text>
-          </TouchableOpacity>
-        </View>
-      )}
+        ) : (
+          <View style={styles.safetyPadding}>
+            <View style={styles.profileContainer}>
+              {profilePicture && (
+                <Image style={styles.profilePicture} source={{ uri: profilePicture }} />
+              )}
+              <View style={styles.legalNameContainer}>
+                <Text style={styles.legalName}>{fName} {mName}</Text>
+                <Text style={styles.bold}>{lName}</Text>
+              </View>
+            </View>
+            <Text style={styles.header}>Credentials</Text>
+            <TouchableOpacity style={styles.button} onPress={redirectToId}>
+              <Text style={styles.buttonText}><Image style={styles.icon} source={carBroomBroom} /> Drivers License</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
@@ -213,29 +109,111 @@ const HomePage = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-          if (route.name === 'Home') {
-            iconName = 'home';
-          } else if (route.name === 'Scan QR') {
-            iconName = 'qr-code';
-          } else if (route.name === 'Settings') {
-            iconName = 'settings';
-          }
-          return <Icon name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: '#a14e61',
-        tabBarInactiveTintColor: '#94737b',
-        headerShown: false,
-      })}
-    >
-      <Tab.Screen name="Home" component={HomePageContent} />
-      <Tab.Screen name="Scan QR" component={ScanQR} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
+    <SafeAreaView style={styles.container}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Scan QR') {
+              iconName = 'qr-code';
+            } else if (route.name === 'Settings') {
+              iconName = 'settings';
+            }
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#a14e61',
+          tabBarInactiveTintColor: '#94737b',
+          tabBarStyle: { position: 'absolute', height: 60 }, // Position the navbar at the bottom
+          headerShown: false,
+        })}
+      >
+        <Tab.Screen name="Home" component={HomePageContent} />
+        <Tab.Screen name="Scan QR" component={ScanQR} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </SafeAreaView>
   );
 };
 
 export default HomePage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e7e6ed',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginTop: 40,
+  },
+  profilePicture: {
+    width: 100,
+    height: 100,
+    borderRadius: 5,
+    marginRight: 20,
+  },
+  legalNameContainer: {
+    flexDirection: 'column',
+  },
+  legalName: {
+    fontSize: 24,
+  },
+  bold: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: '400',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  safetyPadding: {
+    padding: 20,
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#333',
+    flex: 1,
+    paddingLeft: 10,
+    textAlign: 'center',
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+  },
+  bannerColour: {
+    backgroundColor: '#972541',
+    width: '100%',
+    height: 70,
+  },
+  loadingScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e7e6ed',
+  },
+  loadingFont: {
+    fontSize: 16,
+    marginBottom: 50,
+  },
+});
