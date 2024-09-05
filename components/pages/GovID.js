692c9b191dd62a1cf0c43c8f0a32156f1fc7f82b
marcus <marcus@mccarthy.com.au>
2024-09-05 23:07:44 +1000

    testing

--- components/pages/GovID.js
+++ components/pages/GovID.js
@@ -183,154 +183,54 @@ const GovID = () => {
   const safeUpperCase = (text) => (text || "").toUpperCase();
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Animated.Image
-        source={backgroundImage}
-        style={[
-          styles.backgroundImage,
-          { transform: [{ scale: scaleAnim }] },
-        ]}
-      />
-      <ScrollView
-        contentContainerStyle={styles.scrollViewContent}
-      >
-        <View style={styles.digitalID}>
-          <View style={styles.profileContainer}>
-            {profilePicture && (
-              <Image style={styles.profilePicture} source={{ uri: profilePicture }} />
-            )}
-            <View style={styles.textContainer}>
-              {user && (
-                <>
-                  <Text style={styles.unimportantNames}>
-                    {safeUpperCase(user.firstname)} {safeUpperCase(user.middlename)}
-                  </Text>
-                  <Text style={styles.importantLast}>
-                    {safeUpperCase(user.lastname)}
-                  </Text>
-  
-                  <Text style={styles.labelBasicGrey}>DoB</Text>
-                  <Text style={styles.dateOfBirth}>
-                    {user.day} {months[user.month]} {calculate_year_birth(user.age)}
-                  </Text>
-                  <Text style={styles.labelBasicGrey}>Licence No.</Text>
-                  <Text style={styles.licenceNum}>{licenseNum}</Text>
-                </>
-              )}
-            </View>
-          </View>
-          <View style={styles.refreshed}>
-            <View style={styles.refreshTextContainer}>
-              <Text style={styles.refreshLabel}>Information was refreshed online: </Text>
-              <Text style={styles.refreshTime}>{lastRefreshed}</Text>
-            </View>
-            {refreshing && (
-              <View style={styles.loadingContainer}>
-                <Text style={styles.loadingText}>Loading...</Text>
-                <ActivityIndicator size="small" color="#000000" style={styles.loadingIcon} />
-              </View>
-            )}
-          </View>
-
-
-          <View style={styles.divider} />
-  
-          <View style={styles.statusContainer}>
-            <View style={styles.statusTextContainer}>
-              <Text style={styles.statusOutline}>Status ⓘ</Text>
-            </View>
-            <TouchableOpacity style={styles.statusButton}>
-              <Text style={styles.statusButtonText}>Current</Text>
-            </TouchableOpacity>
-          </View>
-
-          <View style={styles.divider} />
-
-          <View style={styles.ageContainer}>
-            <Text style={styles.ageLabel}>Age</Text>
-            <View style={styles.ageStatusContainer}>
-              <Icon name="check-circle" size={35} color="#38c634" style={styles.ageIcon} />
-              <Text style={styles.ageText}>Over 18</Text>
-            </View>
-          </View>
-
-          <View style={styles.divider} />
-  
-          <View style={styles.vehicleInfo}>
-            <View style={styles.vehicleClass}>
-              <Text style={styles.labelBasicGrey}>Class</Text>
-              <Text style={styles.vehicleTextWithIcon}>(C) Car <Icon name="car" size={20} color="#000000" /></Text>
-            </View>
-            <View style={styles.vehicleType}>
-              <Text style={styles.labelBasicGrey}>Type</Text>
-              <Text style={styles.vehicleTextCentered}>(P1) Provisional</Text>
-            </View>
-            <View style={styles.idExpiry}>
-              <Text style={styles.labelBasicGrey}>Expiry</Text>
-              <Text style={styles.vehicleTextCentered}>{expiryDate}</Text>
-            </View>
-          </View>
-  
-          <View style={styles.divider} />
-  
-          <View style={styles.conditions}>
-            <Text style={styles.labelBasicGrey}>Conditions</Text>
-          </View>
-  
-          <View style={styles.divider} />
-          <View style={styles.addressContainer}>
-            <View style={styles.addressTitleContainer}>
-              <Text style={styles.labelBasicGrey}>Address</Text>
-              <Text style={styles.italicText}>Are your details up to {'\n'}date?</Text>
-            </View>
-            <View style={styles.addressInfo}>
-              {user && (
-                <>
-                  <Text>{safeUpperCase(user.houseNumber)} {safeUpperCase(user.street)} {safeUpperCase(user.type)}</Text>
-                  <Text>{safeUpperCase(user.suburb)}</Text>
-                  <Text>QLD {safeUpperCase(user.postCode)}</Text>
-                  <Text>AU</Text>
-                </>
-              )}
-            </View>
-          </View>
-  
-          <View style={styles.divider} />
-          <View style={styles.signatureContainer}>
-            <Text style={styles.statusOutline}>Signature 🔍</Text>
-            <Image style={styles.signature} source={signature} />
-          </View>
-  
-          <View style={styles.divider} />
-  
-          <View style={styles.cardNumberContainer}>
-            <Text style={styles.label}>Card number</Text>
-            <View style={styles.cardNumber}>
-              <Text style={styles.value}>{cardNumber}</Text>
-            </View>
-          </View>
-  
-          <View style={styles.divider} />
-  
-          <View style={styles.countryInfo}>
-            <View style={styles.country}>
-              <Text style={styles.label}>Issuing Country</Text>
-              <Text style={styles.value}>AU</Text>
-            </View>
-            <View style={styles.authority}>
-              <Text style={styles.label}>Issuing Authority</Text>
-              <View style={styles.space} />
-              <Text style={styles.value}>Queensland Government{'\n'}Department of Transport{'\n'}and Main Roads</Text>
-            </View>
-          </View>
-  
-        </View>
-        <View style={styles.bigSpace}>
-
-        </View>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <Text>First Text</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+      
       </ScrollView>
-      <DriverLicenseCard />
-    </SafeAreaView>
+    </View>
   );
   
 };
@@ -338,7 +238,9 @@ const GovID = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: '100%',
+  },
+  scrollViewContent: {
+    flexGrow: 1, // Ensures ScrollView can grow and take up available space
   },
   backgroundImage: {
     ...StyleSheet.absoluteFillObject,
@@ -352,7 +254,6 @@ const styles = StyleSheet.create({
   },
   digitalID: {
     padding: 20,
-    zIndex: 1, // Ensure this is above the background
   },
   profileContainer: {
     flexDirection: 'row',
