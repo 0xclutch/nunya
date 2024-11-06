b870455af1b2f79cc42a40cec5a3b5e012d9ccab
marcus <marcus@mccarthy.com.au>
2024-11-06 10:50:15 +1000

    added links to settings and adjusted position of QLD Gov logo

--- components/pages/Settings.js
+++ components/pages/Settings.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Switch, Button, Linking, SafeAreaView, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons'; // Ensure you have installed react-native-vector-icons
+import React, { useCallback } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Switch, Button, Linking, SafeAreaView, Image, Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
 import { supabase } from '../supabaseClient';
 
-
-const OpenURLButton = ({url}) => {
+const OpenURLButton = ({ url }) => {
   const handlePress = useCallback(async () => {
-    // Checking if the link is supported for links with custom URL scheme.
     const supported = await Linking.canOpenURL(url);
 
     if (supported) {
-      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-      // by some browser in the mobile
       await Linking.openURL(url);
     } else {
       Alert.alert(`Don't know how to open this URL: ${url}`);
     }
   }, [url]);
+
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <Text style={styles.buttonText}>Open Link</Text>
+    </TouchableOpacity>
+  );
 };
-
-
 
 const Settings = ({ navigation }) => {
   const [isEnabled, setIsEnabled] = React.useState(false);
   const [allowNotifications, setAllowNotifications] = React.useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   const toggleNotifications = () => setAllowNotifications(previousState => !previousState);
-  
+
   const qldGovLogo = require('./images/qldgov.png');
-
-
 
   async function registerForPushNotificationsAsync() {
     const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
@@ -50,7 +48,7 @@ const Settings = ({ navigation }) => {
   const LogoutUser = async () => {
     const { error } = await supabase.auth.signOut();
     redirectToLogin();
-  }
+  };
 
   const redirectToLogin = () => {
     navigation.navigate('Login');
@@ -59,7 +57,7 @@ const Settings = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <TouchableOpacity style={styles.button} >
+        <TouchableOpacity style={styles.button}>
           <Icon name="key-outline" size={20} color="#000" />
           <Text style={styles.buttonText}>Change PIN</Text>
           <Icon name="chevron-forward-outline" size={20} color="#000" />
@@ -94,24 +92,24 @@ const Settings = ({ navigation }) => {
           <Text style={styles.buttonText}>Terms and Conditions</Text>
           <Icon name="chevron-forward-outline" size={20} color="#000" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Icon name="globe-outline" size={20} color="#000" onPress={(OpenURLButton("https://www.qld.gov.au/transport/online-services"))} />
+        <TouchableOpacity style={styles.button} onPress={() => OpenURLButton({ url: "https://www.qld.gov.au/transport/online-services" })}>
+          <Icon name="globe-outline" size={20} color="#000" />
           <Text style={styles.buttonText}>Access TMR Online Services</Text>
           <Icon name="chevron-forward-outline" size={20} color="#000" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Icon name="person-circle-outline" size={20} color="#000" onPress={(OpenURLButton("https://identity.qld.gov.au/login/login.html"))}/>
+        <TouchableOpacity style={styles.button} onPress={() => OpenURLButton({ url: "https://identity.qld.gov.au/login/login.html" })}>
+          <Icon name="person-circle-outline" size={20} color="#000" />
           <Text style={styles.buttonText}>Manage your Queensland Digital Identity</Text>
           <Icon name="chevron-forward-outline" size={20} color="#000" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.feedbackButton} onPress={(OpenURLButton("https://www.transporttalk.tmr.qld.gov.au/jfe/form/SV_8BpqoBULWNnlKZw"))}>
+        <TouchableOpacity style={styles.feedbackButton} onPress={() => OpenURLButton({ url: "https://www.transporttalk.tmr.qld.gov.au/jfe/form/SV_8BpqoBULWNnlKZw" })}>
           <Text style={styles.feedbackText}>GIVE US YOUR FEEDBACK</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.logout} onPress={(LogoutUser())}>
+        <TouchableOpacity style={styles.logout} onPress={LogoutUser}>
           <Text style={styles.logoutText}>LOGOUT</Text>
         </TouchableOpacity>
         <Image source={qldGovLogo} style={styles.logo} />
-          <Text style={styles.details}>Digital License App {'\n'}Copyright© State of Queensland 2024{'\n'}Version No: 2.19.0 (6636)</Text>
+        <Text style={styles.details}>Digital License App {'\n'}Copyright© State of Queensland 2024{'\n'}Version No: 2.19.0 (6636)</Text>
       </ScrollView>
     </SafeAreaView>
   );
@@ -120,29 +118,29 @@ const Settings = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff', // Set background to white
+    backgroundColor: '#ffffff',
   },
   scrollViewContent: {
-    flexGrow: 1, // Ensures ScrollView can grow and take up available space
+    flexGrow: 1,
   },
   button: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#fff', // White background for buttons
-    borderBottomColor: '#ccc', // Light grey border color
+    backgroundColor: '#fff',
+    borderBottomColor: '#ccc',
     borderBottomWidth: 1,
     paddingVertical: 15,
     paddingHorizontal: 20,
   },
   buttonText: {
     fontSize: 16,
-    color: '#333', // Dark text for button
-    flex: 1, // Take up available space between icons
-    paddingLeft: 10, // Space after the icon
+    color: '#333',
+    flex: 1,
+    paddingLeft: 10,
   },
   feedbackButton: {
-    backgroundColor: '#e74c3c', // Matching red background for feedback button
+    backgroundColor: '#e74c3c',
     marginVertical: 20,
     marginHorizontal: 20,
     padding: 15,
@@ -150,12 +148,12 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   feedbackText: {
-    color: '#fff', // White text for feedback
+    color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
   },
   logout: {
-    backgroundColor: '#c0392b', // Darker red for the logout button
+    backgroundColor: '#c0392b',
     padding: 15,
     marginHorizontal: 20,
     alignItems: 'center',
@@ -163,20 +161,19 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   logoutText: {
-    color: '#fff', // White text for logout
+    color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
   },
   logo: {
-    width: 150, // Adjust width as needed
-    height: 150, // Adjust height as needed
+    width: 150,
+    height: 150,
     resizeMode: 'contain',
     alignSelf: 'center',
     marginVertical: 20,
   },
   details: {
     textAlign: "center",
-
   }
 });
 
